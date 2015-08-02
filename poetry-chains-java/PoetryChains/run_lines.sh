cf0909d100439ab258222bc38882ebae4f6f22cd
angusforbes <angusforbes>
2015-08-02 15:27:31 -0500

    removes sudo requirement (!)

--- poetry-chains-java/PoetryChains/run_lines.sh
+++ poetry-chains-java/PoetryChains/run_lines.sh
@@ -1,2 +1,2 @@
-sudo javac -cp .:behaviorism.jar:google-collect-1.0.jar  *.java
-sudo java -cp .:behaviorism.jar:google-collect-1.0.jar LineMaker
+javac -cp .:behaviorism.jar:google-collect-1.0.jar  *.java
+java -cp .:behaviorism.jar:google-collect-1.0.jar LineMaker
