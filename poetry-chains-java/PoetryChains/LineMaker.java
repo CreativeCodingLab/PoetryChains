b025e8cfc361238b0df4d72337fafd1d2a87d145
Paul Murray <plmrry@gmail.com>
2015-08-04 01:48:29 -0500

    minor change

--- poetry-chains-java/PoetryChains/LineMaker.java
+++ poetry-chains-java/PoetryChains/LineMaker.java
@@ -9,7 +9,7 @@ public class LineMaker {
 
   static boolean OUTPUT_JSON = true;
   static int MAX_LINES = 20; //max collocated lines per iteration
-  static int NUM_LINES = 4; //num iterations
+  static int NUM_LINES = 3; //num iterations
 
   public Word lastSelected = null;
 
